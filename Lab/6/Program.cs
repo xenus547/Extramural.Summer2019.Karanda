abd9b4dea80de97278022ac34b742791057193ab
xenus547@gmail.com <xenus547>
2019-12-11 16:14:28 +0300

    6 done

---
+++ Lab/6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Logic;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string:");
+            string str = Console.ReadLine();
+            Console.WriteLine(str.ReverceString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab/Logic/StrLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    public static class StrLogic
+    {
+        public static string ReverceString(this string str)
+        {
+            StringBuilder sb = new StringBuilder(str.Length);
+            for (int i = str.Length; i-- != 0;)
+                sb.Append(str[i]);
+            return sb.ToString();
+        } 
+    }
+}
