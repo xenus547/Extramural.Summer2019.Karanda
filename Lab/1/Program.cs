0befd8eb3afa867ca6dac28dd9f33910e5d45847
xenus547@gmail.com <xenus547>
2019-12-11 09:36:22 +0300

    1 done

---
+++ Lab/1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Logic;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter b:");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine($"First number: {a}");
+            Console.WriteLine($"First number: {b}");
+
+            NumbericManipulation.Swap(ref a, ref b);
+
+            Console.WriteLine($"First number: {a}");
+            Console.WriteLine($"First number: {b}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab/Logic/NumbericManipulation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Logic
+{
+    public class NumbericManipulation
+    {
+        public static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T t = lhs;
+            lhs = rhs;
+            rhs = t;
+        }
+    }
+}
