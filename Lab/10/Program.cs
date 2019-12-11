797e2da498fc149fee63d93e5d72ef9abf6b2e41
xenus547@gmail.com <xenus547>
2019-12-11 17:58:29 +0300

    10

---
+++ Lab/10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Logic;
+
+namespace _10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter N:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n.primeNum());
+            Console.ReadKey();
+        }
+    }
+}
--- Lab/Logic/NumbericManipulation.cs
+++ Lab/Logic/NumbericManipulation.cs
@@ -1,14 +1,59 @@
 ﻿using System;
+using System.Text;
 
 namespace Logic
 {
-    public class NumbericManipulation
+    public static class NumbericManipulation
     {
         public static void Swap<T>(ref T lhs, ref T rhs)
         {
             T t = lhs;
             lhs = rhs;
             rhs = t;
+        }
+
+        public static string primeNum(this int n)
+        {
+            var sb = new StringBuilder();
+            var count = 0u;
+            var i = 0u;
+            while (count < n)
+            {
+                if (IsPrimeNumber(i))
+                {
+                    sb.Append(i);
+                    if (count < n - 1)
+                    {
+                        sb.Append(", ");
+                    }
+                    count++;
+                }
+                i++;
+            }
+            return sb.ToString();
+        }
+
+        public static bool IsPrimeNumber(uint n)
+        {
+            var result = true;
+
+            if (n > 1)
+            {
+                for (var i = 2u; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
         }
     }
 }
