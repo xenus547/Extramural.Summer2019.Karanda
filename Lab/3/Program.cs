1ca5ca8a09dc16ab8e9bbdcf86b2b30030e1b374
xenus547@gmail.com <xenus547>
2019-12-11 09:51:19 +0300

    3 done

---
+++ Lab/3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Logic;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter decimial:");
+            int dec = int.Parse(Console.ReadLine());
+            Console.WriteLine(dec.ToB());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab/Logic/ToBinary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    public static class ToBinary
+    {
+        public static string ToB(this int value)
+        {
+            var builder = new StringBuilder();
+
+            char sign = value >= 0 ? ' ' : '-';
+
+            value = Math.Abs(value);
+
+            while (value != 0)
+            {
+                char unit = value % 2 == 0 ? '0' : '1';
+
+                builder.Append(unit);
+
+                value /= 2;
+            }
+
+            builder.Append(sign);
+
+            for (int i = 0; i < builder.Length / 2; i++)
+            {
+                char tmp = builder[i];
+
+                builder[i] = builder[builder.Length - i - 1];
+
+                builder[builder.Length - i - 1] = tmp;
+            }
+
+            return builder.ToString();
+        }
+    }
+}
