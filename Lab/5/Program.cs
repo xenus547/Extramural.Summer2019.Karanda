7ba51c8177b5d2763e02aeb385fdc8647389ca26
xenus547@gmail.com <xenus547>
2019-12-11 15:56:31 +0300

    5 done

---
+++ Lab/5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Logic;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the latitude of coordinate 1:");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Input the longitude of coordinate 1:");
+            double y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Input the latitude of coordinate 2:");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Input the longitude of coordinate 2:");
+            double y2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("The distance between those points is: "+Distance.distanceBetween(x1,x2,y1,y2)+" km");
+            Console.ReadKey();
+        }
+    }
+}
