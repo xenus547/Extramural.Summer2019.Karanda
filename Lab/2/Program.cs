22ff534267825d0f4835838c467afbcf665d7a00
xenus547@gmail.com <xenus547>
2019-12-11 09:45:59 +0300

    2 done

---
+++ Lab/2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Logic;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CreateFace.CreateF());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab/Logic/CreateFace.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    public class CreateFace
+    {
+        public static string CreateF()
+        {
+            var builder = new StringBuilder();
+            builder.Append("\n +\"\"\"\"\"+");
+            builder.Append("\n(| o o |)");
+            builder.Append("\n |  ^  |");
+            builder.Append("\n | '-' |");
+            builder.Append("\n +-----+");
+
+            return builder.ToString();
+        }
+    }
+}
